946c0e9302e957a9afd7c7b21dcf7c85a88613e0
Ivan Golovinov <topclassprogrammer@gmail.com>
2024-01-23 23:21:09 +0300

    Added Words model

--- models.py
+++ models.py
@@ -11,3 +11,16 @@ class Users(Base):
     user_id = Column(Integer, primary_key=True)
     telegram_id = Column(Integer, unique=True, nullable=False)
     words = relationship('Users_words', back_populates='users')
+
+
+class Words(Base):
+    """Таблица английских слов, их переводов на русский язык и
+    наличия флага принадлежности к общему словарю пользователей"""
+    __tablename__ = 'words'
+    word_id = Column(Integer, primary_key=True)
+    eng_word = Column(String(40), nullable=False)
+    rus_word = Column(String(40), nullable=False)
+    common_word = Column(Boolean, nullable=False)
+    users = relationship('Users_words', back_populates='words')
+
+
