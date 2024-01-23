74ae245f9e40b551e1711243278496a5589eb547
Ivan Golovinov <topclassprogrammer@gmail.com>
2024-01-23 23:22:31 +0300

    Added Users_words model

--- models.py
+++ models.py
@@ -24,3 +24,16 @@ class Words(Base):
     users = relationship('Users_words', back_populates='words')
 
 
+class Users_words(Base):
+    """Промежуточная таблица для связи таблиц пользователей и слов
+    при отношении многие-ко-многим"""
+    __tablename__ = 'users_words'
+    user_id = Column(Integer, ForeignKey('users.user_id', ondelete="CASCADE"),
+                     primary_key=True)
+    word_id = Column(Integer, ForeignKey('words.word_id', ondelete="CASCADE"),
+                     primary_key=True)
+    words = relationship('Words', back_populates='users')
+    users = relationship('Users', back_populates='words')
+
+
+
